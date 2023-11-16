b57b3abecf567d4e8710f53d2a200d2e4c6c4e9b
Felipe Padovani <padovani1010@gmail.com>
2023-11-16 18:44:33 -0300

    header finalizado

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+import Swal from 'sweetalert2';
+
+const btnSearch = document.querySelector('#search-button');
+
+btnSearch.addEventListener('click', () => {
+    const city = document.querySelector('#input-city').value;
+});
