63d68bae91838612383b47f2900ac663ce5e7495
Felipe Padovani <padovani1010@gmail.com>
2023-11-17 16:51:43 -0300

    css do box temperature em andamento

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import './style.css';
 import Swal from 'sweetalert2';
+import weather from './weather.js'
 
 const btnSearch = document.querySelector('#search-button');
+const weatherContainer = document.querySelector('#weather-container');
 
-btnSearch.addEventListener('click', () => {
+btnSearch.addEventListener('click', async () => {
     const city = document.querySelector('#input-city').value;
+    try{
+        const weatherFromCity = await weather(city);
+        updateInfoContainer(weatherFromCity);
+    } catch(error){
+        Swal.fire({
+            icon: 'error',
+            title:'Ops ... Algo deu errado.',
+            text : error.message,
+        });
+    }
 });
+
+const updateInfoContainer = (objData) => {
+          
+}   
