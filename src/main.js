17909e90fcda73697039a7f10c9ed8345f7e69ab
Felipe Padovani <padovani1010@gmail.com>
2023-11-17 20:24:02 -0300

    Alternando criação do weather container para js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import Swal from 'sweetalert2';
 import weather from './weather.js'
 
 const btnSearch = document.querySelector('#search-button');
-const weatherContainer = document.querySelector('#weather-container');
+const cityOutput = document.querySelector('#city');
+const countryOutput = document.querySelector('#country');
+const tempOutput = document.querySelector('#temp');
+const descriptionOutput = document.querySelector('#temp-description');
+const imgIconOutput = document.querySelector('#img-icon');
 
 btnSearch.addEventListener('click', async () => {
+    document.querySelector('#weather-container').style.display = 'block';
     const city = document.querySelector('#input-city').value;
     try{
         const weatherFromCity = await weather(city);
@@ -20,5 +25,9 @@ btnSearch.addEventListener('click', async () => {
 });
 
 const updateInfoContainer = (objData) => {
-          
+    cityOutput.innerText = objData.address;
+    countryOutput.innerText = objData.resolvedAddress.split(',')[2];
+    tempOutput.innerText = objData.currentConditions.temp;
+    descriptionOutput.innerText = objData.currentConditions.conditions;
+    imgIconOutput.src = `./images/icons/${objData.currentConditions.icon}.png`;
 }   
--- src/weather.js
+++ src/weather.js
@@ -2,16 +2,5 @@ export default async function weather(city) {
     if(!city) throw new Error('Você precisa inserir uma cidade.');
     const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=S6AJYHB94VLKJEEDLQBKMXC55&contentType=json`)
     const data = await response.json();
-    // const objWeather = filterData(data);
-    // return objWeather;
     return data;
-}
-
-const filterData = (data) => {
-    return({
-        adress : data.address,
-        description : data.description,
-        temp : data.days[0].temp,
-        icon : data.days[0].icon,
-    }) 
 }
