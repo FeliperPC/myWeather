556eb60f314db37bb12388aa2d3402e90b668efe
Felipe Padovani <padovani1010@gmail.com>
2023-11-20 16:43:13 -0300

    create forecast element em execução

--- src/main.js
+++ src/main.js
@@ -11,16 +11,17 @@ const imgIconOutput = document.querySelector('#img-icon');
 const weatherContainer = document.querySelector('#weather-container');
 const btnForecast = document.querySelector('#btn-forecast');
 
-const days = ['Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'];
+const days = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
 
 btnSearch.addEventListener('click', async () => {
     const cityWeather = await getAPIweather();
     createWeatherContainer(cityWeather);
 });
 
-btnForecast.addEventListener('click',async ()=>{
-   const cityWeather = await getAPIweather();
-   forecastFilter(cityWeather);
+btnForecast.addEventListener('click', async () => {
+    const cityWeather = await getAPIweather();
+    const daysForecast = forecastContainerDays(cityWeather);
+    createForecastContainer(daysForecast);
 });
 
 const createWeatherContainer = (objData) => {
@@ -30,23 +31,40 @@ const createWeatherContainer = (objData) => {
     tempOutput.innerText = objData.currentConditions.temp;
     descriptionOutput.innerText = objData.currentConditions.conditions;
     imgIconOutput.src = `./images/icons/${objData.currentConditions.icon}.png`;
-}   
+}
 
 const getAPIweather = async () => {
     const city = document.querySelector('#input-city').value;
-    try{
+    try {
         const cityWeather = await weather(city);
         return cityWeather;
-    } catch(error){
+    } catch (error) {
         Swal.fire({
             icon: 'error',
-            title:'Ops ... Algo deu errado.',
-            text : error.message,
+            title: 'Ops ... Algo deu errado.',
+            text: error.message,
         });
     }
-} 
+}
 
-const forecastFilter = (weatherData) => {
+const forecastContainerDays = (weatherData) => {
     const data = new Date(weatherData.days[0].datetime);
-    
+    const arrayDays = [];
+    for (let i = 1; i < 4; i += 1) {
+        const objDay = {
+            datetime: weatherData.days[i].datetime,
+            tempmin: weatherData.days[i].tempmin,
+            tempmax: weatherData.days[i].tempmax,
+            conditions: weatherData.days[i].conditions,
+            icon: weatherData.days[i].icon,
+        }
+        arrayDays.push(objDay);
+    }
+    return arrayDays;
+}
+
+const createForecastContainer = (days) => {
+    days.forEach((day)=>{
+        /// Criar 3 elementos um de cada lado
+    })
 }
