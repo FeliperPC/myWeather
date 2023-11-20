5973471ebe49e7f1f26b7a2bebaaf931478891aa
Felipe Padovani <padovani1010@gmail.com>
2023-11-20 15:07:56 -0300

    forecast funtion em execucao

--- src/main.js
+++ src/main.js
@@ -8,13 +8,35 @@ const countryOutput = document.querySelector('#country');
 const tempOutput = document.querySelector('#temp');
 const descriptionOutput = document.querySelector('#temp-description');
 const imgIconOutput = document.querySelector('#img-icon');
+const weatherContainer = document.querySelector('#weather-container');
+const btnForecast = document.querySelector('#btn-forecast');
+
+const days = ['Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'];
 
 btnSearch.addEventListener('click', async () => {
-    document.querySelector('#weather-container').style.display = 'block';
+    const cityWeather = await getAPIweather();
+    createWeatherContainer(cityWeather);
+});
+
+btnForecast.addEventListener('click',async ()=>{
+   const cityWeather = await getAPIweather();
+   forecastFilter(cityWeather);
+});
+
+const createWeatherContainer = (objData) => {
+    weatherContainer.style.display = 'flex';
+    cityOutput.innerText = objData.address;
+    countryOutput.innerText = objData.resolvedAddress.split(',')[2];
+    tempOutput.innerText = objData.currentConditions.temp;
+    descriptionOutput.innerText = objData.currentConditions.conditions;
+    imgIconOutput.src = `./images/icons/${objData.currentConditions.icon}.png`;
+}   
+
+const getAPIweather = async () => {
     const city = document.querySelector('#input-city').value;
     try{
-        const weatherFromCity = await weather(city);
-        updateInfoContainer(weatherFromCity);
+        const cityWeather = await weather(city);
+        return cityWeather;
     } catch(error){
         Swal.fire({
             icon: 'error',
@@ -22,12 +44,9 @@ btnSearch.addEventListener('click', async () => {
             text : error.message,
         });
     }
-});
+} 
 
-const updateInfoContainer = (objData) => {
-    cityOutput.innerText = objData.address;
-    countryOutput.innerText = objData.resolvedAddress.split(',')[2];
-    tempOutput.innerText = objData.currentConditions.temp;
-    descriptionOutput.innerText = objData.currentConditions.conditions;
-    imgIconOutput.src = `./images/icons/${objData.currentConditions.icon}.png`;
-}   
+const forecastFilter = (weatherData) => {
+    const data = new Date(weatherData.days[0].datetime);
+    
+}
