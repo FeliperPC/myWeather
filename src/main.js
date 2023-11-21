eb80bd572182fa349cbbdf3ceb811e2deac5bed0
Felipe Padovani <padovani1010@gmail.com>
2023-11-20 22:17:32 -0300

    tradução dos dados da api para português

--- src/main.js
+++ src/main.js
@@ -10,8 +10,9 @@ const descriptionOutput = document.querySelector('#temp-description');
 const imgIconOutput = document.querySelector('#img-icon');
 const weatherContainer = document.querySelector('#weather-container');
 const btnForecast = document.querySelector('#btn-forecast');
+const forecastContainer = document.querySelector('.forecast-container');
 
-const days = ['Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado', 'Domingo'];
+const days = ['Seg.', 'Ter.', 'Qua.', 'Qui.', 'Sex.', 'Sáb.', 'Dom.'];
 
 btnSearch.addEventListener('click', async () => {
     const cityWeather = await getAPIweather();
@@ -63,8 +64,30 @@ const forecastContainerDays = (weatherData) => {
     return arrayDays;
 }
 
-const createForecastContainer = (days) => {
-    days.forEach((day)=>{
-        /// Criar 3 elementos um de cada lado
-    })
+const createForecastContainer = (daysAPI) => {
+    daysAPI.forEach((day)=>{
+        const dayBox = document.createElement('div');
+        const dayOfTheWeek = new Date(day.datetime);
+        const dayOutput = document.createElement('h1');
+        dayOutput.id = 'forecast-day';
+        dayOutput.innerText = days[dayOfTheWeek.getDay()];
+        const tempMax = document.createElement('p');
+        tempMax.innerText =`máx : ${day.tempmax}`;
+        tempMax.style.fontWeight ='bold';
+        const tempMin = document.createElement('p');
+        tempMin.innerText = `min : ${day.tempmin}`;
+        const img = document.createElement('img');
+        img.src = `./images/icons/${day.icon}.png`;
+        const condition = document.createElement('p');
+        condition.innerText = day.conditions;
+        condition.id = 'condition-forecast';
+
+        dayBox.appendChild(dayOutput);
+        dayBox.appendChild(tempMax);
+        dayBox.appendChild(tempMin);
+        dayBox.appendChild(condition);
+        forecastContainer.appendChild(dayBox);
+        forecastContainer.appendChild(img);
+    });
+    forecastContainer.style.display='flex';
 }
